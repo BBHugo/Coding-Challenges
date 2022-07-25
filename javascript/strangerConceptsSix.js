610d2249662b36c2665d8fde18a4ab905ac2b3c9
Hugo Mendoza <thehugomendoza@gmail.com>
2022-07-24 22:32:38 -0700

    adding switch

--- javascript/strangerConceptsSix.js
+++ javascript/strangerConceptsSix.js
@@ -41,4 +41,14 @@ function likes(names){
   }
 }
 
- //The one below will be the same output of code, however done in switch conditionals instead.
+ //The one below will be the same output of code, however done in switch conditionals instead. We can compare one way of doing it to the next more simply.
+ function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
