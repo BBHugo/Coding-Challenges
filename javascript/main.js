4d2d94c6d86b732b8feb1b375a4cfde05f2dacb6
BBHugo <thehugomendoza@gmail.com>
2022-08-22 21:45:25 -0700

    Added pulling instructions for future use in main.js

---
+++ javascript/main.js
@@ -0,0 +1,3 @@
+//A reminder that "git pull" will fetch the remote repo and update our local repo at the same time.
+
+//"git fetch" will only show us the changes that were made for us to use it, I believe we merge it with git merge but not sure just yet.
