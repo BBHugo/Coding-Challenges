61bbe776e110777d9c0998c0ca644743e847742c
BBHugo <thehugomendoza@gmail.com>
2023-01-02 15:54:49 -0800

    added big example for spread operator and sorting a new Set

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,9 @@
 //A reminder that "git pull" will fetch the remote repo and update our local repo at the same time.
 
 //"git fetch" will only show us the changes that were made for us to use it, I believe we merge it with git merge but not sure just yet.
+
+//Using Spread Operators and new Set method WITH the sort method, we're able to combine the given parameter arrays into one, sort them by number (They're all integers in this exercise) and remove any duplicates with the new Set.
+function mergeArrays(arr1, arr2) {
+    let sortedM = [...arr1, ...arr2].sort((a,b) => a-b) //Sorts the array as well as merging all arrays using spread operator
+    return [...new Set(sortedM)] //Creates a new Set which removes the duplicates
+  }
